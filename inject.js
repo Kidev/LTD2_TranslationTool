551022b276354614eb2160194cccc121a76f40f3
Alexandre 'Kidev' Poumaroux <1204936+Kidev@users.noreply.github.com>
2023-01-11 22:42:35 +0100

    Closer front end

--- inject.js
+++ inject.js
@@ -35,8 +35,8 @@ function dragElement(elmnt) {
 	
 	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 	
-	if (document.getElementById(elmnt.id + "header")) {
-		document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+	if (document.getElementById(elmnt.id + "Header")) {
+		document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
 	} else {
 		elmnt.onmousedown = dragMouseDown;
 	}
