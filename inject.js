5f9056794cc35bafb2ca9cacfd06a37a1d7822f5
Alexandre 'Kidev' Poumaroux <1204936+Kidev@users.noreply.github.com>
2023-01-18 21:56:10 +0100

    Should fix the host permission requirement

--- inject.js
+++ inject.js
@@ -82,89 +82,13 @@ function injectCSSSheet(sheetContent) {
 
 function onWindowReady() {
 
-	//injectCSS("https://use.fontawesome.com/releases/v5.0.6/css/all.css");
-	//injectCSS("https://developer.legiontd2.com/black/css/nucleo-icons.css");
-	/*injectCSS("https://developer.legiontd2.com/css/btf.css");
-	injectCSS("https://developer.legiontd2.com/css/style.css");
-	injectCSS("https://developer.legiontd2.com/css/flag-icon.css");
-	injectCSS("https://developer.legiontd2.com/black/css/black-dashboard.css?v=1.0.3");
-	injectCSS("https://developer.legiontd2.com/black/css/theme.css");*/
-	/*
-	injectCSSSheet("" +
-		"@font-face {\n" +
-		"    font-family: 'Poppins';\n" +
-		"    font-style: normal;\n" +
-		"    font-weight: 400;\n" +
-		"    src: url(" + chrome.runtime.getURL("font/Poppins-Regular.ttf") + ") format('ttf');\n" +
-		"    unicode-range: U+0900-097F, U+1CD0-1CF6, U+1CF8-1CF9, U+200C-200D, U+20A8, U+20B9, U+25CC, U+A830-A839, U+A8E0-A8FB;\n" +
-		"}\n" +
-		"#dragDiv {\n" +
-		"    position: absolute;\n" +
-		"    z-index: 9;\n" +
-		"    background-color: transparent; \n" + //*red;/*#1d1d2e;
-		"    border-color: red;\n" +
-		"    text-align: left;\n" + // border: 1px solid #d3d3d3;
-		"    top: 5%;\n" +
-		"    left: 5%;\n" +
-		"    width: auto;\n" +
-		"    max-width: 400px;\n" +
-		"    max-height: 1000px;\n" +
-		"    height: auto;\n" +
-		"    font-family: poppins,sans-serif;\n" +
-		"    box-sizing: border-box;\n" +
-		"    border-radius: 100px;\n" +
-		"    padding-top: -10px;\n" +
-		"}\n" +
-		"#dragDivHeader {\n" +
-		"    width: auto;\n" +
-		"    height: 0;\n" +
-		"    cursor: move;\n" +
-		"    z-index: 10;\n" +
-		"    color: white;\n" +
-		"    font-family: poppins,sans-serif;\n" +
-		"    text-align: left;\n" +
-		"    background-color: \n" + //#27293d;
-		"    border-color: 100px;\n" + //rgba(100, 1000, 0, 200;
-		"    border-top: -50px;\n" +
-		"    padding: -5px;\n" +
-		"    text-transform: uppercase;\n" +
-		"    font-size: 1rem;\n" +
-		"}\n" +
-		"#dragDivTranslation {\n" +
-		"    width: 100%;\n" +
-		"    height: 100%;\n" +
-		"    background-color: #27293d;\n" +
-		"    color: white;\n" +
-		"    border-color: transparent;\n" +
-		"    border-bottom: 5px;\n" +
-		"    overflow-y:auto;\n" +
-		"    box-sizing: border-box;\n" +
-		"    font-size: 14px;\n" +
-		"    font-weight: 700;\n" +
-		"    line-height: 21px;\n" +
-		"    text-align: left;\n" +
-		"    text-transform: none;\n" +
-		"    -webkit-font-smoothing: antialiased;\n" +
-		"    overflow-wrap: break-word;\n" +
-		"    user-select: none;\n" +
-		"    -webkit-tap-highlight-color: rgba(0,0,0,0);\n" +
-		"    padding: 5px;\n" +
-		"}\n" +
-		".apex-data{\n" +
-		"    margin: 2px;\n" +
-		"    padding: 0 5px;\n" +
-		"    display:inline-block;\n" +
-		"    background-color: #464646;\n" +
-		"    border: 1px dashed #707070;\n" +
-		"}"
-	);*/
-
 	var div = document.createElement("div");
 	div.setAttribute("id", "dragDiv");
 	document.body.append(div);
 
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', chrome.runtime.getURL("preview_box.html"), true);
+	console.log(chrome.runtime.getURL("preview_box.html"));
 	xhr.onreadystatechange = function()
 	{
 		if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
