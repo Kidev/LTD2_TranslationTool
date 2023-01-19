8c9f0d1d6ee084910499672e6935ea1d2973df4d
Alexandre 'Kidev' Poumaroux <1204936+Kidev@users.noreply.github.com>
2023-01-19 02:27:59 +0100

    Remove autoload to ease the permissions requirements of the tool

--- background.js
+++ background.js
@@ -1,6 +1,31 @@
+function injectCSSFile(path) {
+	var link = document.createElement('link');
+	link.rel = 'stylesheet';
+	link.type = 'text/css';
+	link.href = path;
+	document.head.appendChild(link);
+}
+
 function toggleDiv() {
+
 	var element = document.getElementById("dragDiv");
-	if (typeof element !== 'undefined' && element !== null) {
+
+	if (element === null || typeof element === 'undefined') {
+		for (var cssPath in [ "inject.css",
+			                  "css/btf.css",
+							  "css/black-dashboard.css",
+							  "css/flag-icon.css",
+							  "css/style.css",
+							  "css/theme.css",
+							  "css/empty.css" ]) {
+			injectCSSFile(cssPath);
+		}
+
+		chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			files: ["inject.js"]
+		});
+	} else {
 		if (element.style.display === "none") {
 			element.style.display = "block";
 		} else {
@@ -10,11 +35,10 @@ function toggleDiv() {
 }
 
 chrome.action.onClicked.addListener((tab) => {
-	console.log('#' + chrome.runtime.getURL('font/Poppins-Regular.ttf') + '#');
 	if (tab.url.includes("https://docs.google.com/spreadsheets/")) {
 		chrome.scripting.executeScript({
 			target: { tabId: tab.id },
 			function: toggleDiv
-	});
-  }
+		});
+	}
 });
