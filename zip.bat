279fce1d84e43ac5d32d44ee751abf4468ef3bcc
Alexandre 'Kidev' Poumaroux <1204936+Kidev@users.noreply.github.com>
2023-01-20 07:17:48 +0100

    Fix script

--- zip.bat
+++
@@ -1,2 +0,0 @@
-powershell -Command "Get-ChildItem -Path .\build\firefox\* -Recurse | Compress-Archive -Recurse  -DestinationPath .\build\CoachTranslateTool_firefox.zip"
-powershell -Command "Get-ChildItem -Path .\build\chrome\* -Recurse | Compress-Archive -Recurse  -DestinationPath .\build\CoachTranslateTool_chrome.zip"
