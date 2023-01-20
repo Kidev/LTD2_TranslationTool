467eb260400a42f73fbbe3099270bb0bed435ccc
Alexandre 'Kidev' Poumaroux <1204936+Kidev@users.noreply.github.com>
2023-01-21 00:52:02 +0100

    Remove old folder remains

--- build.bat
+++ build.bat
@@ -1,12 +1,5 @@
-powershell -Command "del .\build -Force -Recurse"
 powershell -Command "del .\CoachTranslateTool_firefox.zip -Force"
 powershell -Command "del .\CoachTranslateTool_chrome.zip -Force"
 
-powershell -Command "mkdir .\build\firefox"
-powershell -Command "mkdir .\build\chrome"
-
-powershell -Command "Copy-Item -Recurse -Path .\firefox\* -Destination .\build\firefox"
-powershell -Command "Copy-Item -Recurse -Path .\chrome\* -Destination .\build\chrome"
-
-powershell -Command "Compress-Archive -Path .\build\firefox\* -DestinationPath .\CoachTranslateTool_firefox.zip"
-powershell -Command "Compress-Archive -Path .\build\chrome\* -DestinationPath .\CoachTranslateTool_chrome.zip"
+powershell -Command "Compress-Archive -Path .\firefox\* -DestinationPath .\CoachTranslateTool_firefox.zip"
+powershell -Command "Compress-Archive -Path .\chrome\* -DestinationPath .\CoachTranslateTool_chrome.zip"
