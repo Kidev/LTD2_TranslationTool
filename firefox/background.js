b0b9e09467c819b97861fae7f8f9c4644a53f81c
Alexandre 'Kidev' Poumaroux <1204936+Kidev@users.noreply.github.com>
2023-01-22 04:21:01 +0100

    Update firefox worker

--- firefox/background.js
+++ firefox/background.js
@@ -2,11 +2,14 @@ browser.action.onClicked.addListener(async (tab) => {
     if (tab.url.includes("https://docs.google.com/spreadsheets/")) {
         try {
             await browser.scripting.executeScript({
-                target: { tabId: tab.id },
-                files: ["inject.js"]
+                target: {
+                    tabId: tab.id,
+                    allFrames: true
+                },
+                files: ["inject.js"].map((source) => browser.runtime.getURL(source))
             });
         } catch (err) {
             console.error(`Failed to inject script (${err})`);
         }
     }
-}, {});
+});
